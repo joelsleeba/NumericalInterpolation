097e02d5d1044015c2b1dbe87f4c3e70fc40b95c
joelsleeba <joelsleeba1@gmail.com>
2022-01-16 12:28:55 +0530

    gaussian_elimination

---
+++ gaussian_elimination.m
@@ -0,0 +1,13 @@
+A = [1 2 3; 4 5 6; 7 8 9];
+gauss_elim(A)
+
+function A = gauss_elim(A)
+    for i = 1:length(A)-1
+        for j = i+1:length(A)
+            d = A(j, i)/A(i, i);
+            for k = i:length(A(j, :))
+                A(j, k) = A(j, k) - d*A(i, k);
+            end
+        end
+    end
+end
