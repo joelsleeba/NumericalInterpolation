5d89e7903a1d484212720356b7d529f9b77c4596
joelsleeba <joelsleeba1@gmail.com>
2022-01-31 11:28:55 +0530

    bookkeeping

--- eigenvalues.m
+++ eigenvalues.m
@@ -1,14 +1,17 @@
 A = [2 0 -1; -2 -10 0; -1 -1 4];
-f = @(A) A(1);
+f = @(x) x(2);
 x = [1 1 1];
-power_method(A, x, f, 10);
+[eigvec, eigval] = power_method(A, x, f, 100);
+fprintf("eigenvector: ")
+disp(eigvec);
+fprintf("eigenvalue: ")
+disp(eigval);
 
-function power_method(A, x, f, iter)
+function [x, r] = power_method(A, x, f, iter)
+    r = 0;
     for i = 1:iter
-        y = (A*(x)')';
+        y = (A*(x'))';
         r = f(y)/f(x);
         x = y;
-        disp(x);
-        disp(r);
     end
 end
