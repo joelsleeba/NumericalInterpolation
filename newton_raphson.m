05835cd6abf0e36d504a637f71624e43a193bab9
joelsleeba <joelsleeba1@gmail.com>
2022-02-09 12:37:17 +0530

    bug fix
    Signed-off-by: joelsleeba <joelsleeba1@gmail.com>

--- newton_raphson.m
+++ newton_raphson.m
@@ -2,9 +2,9 @@ tol = 1E-6;
 p0 = 1.5;
 f = @(x) x^3 + 4*x^2 -10;
 fp = @(x) 3*x^2 + 8*x;
-newton_rhap(f, fp, p0, tol, 1000)
+newton_rap(f, fp, p0, tol, 1000)
 
-function root = newton_rhap(f, fp, x, tol, itermax)
+function root = newton_rap(f, fp, x, tol, itermax)
     for i = 1:itermax
         temproot = x - f(x)/fp(x);
         if abs(temproot - x) < tol
--- newton_raphson.py
+++ newton_raphson.py
@@ -3,9 +3,9 @@ import math
 f = lambda x: x**3 -5*x +1
 f1 = lambda x: 3*x**2 - 5
 
-def newton_rhapson(x):
+def newton_raphson(x):
     for i in range(12):
         print(x)
         x =  x - f(x)/f1(x)
 
-newton_rhapson(0)
+newton_raphson(0)
